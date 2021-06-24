b2fc35ae4d45d0012a1200933519de797d81d225
Ashutosh Dixit <ashutoshd2001@gmail.com>
2021-06-24 16:30:58 +0530

    modified home controlerr

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,3 +2,6 @@ module.exports.home=function(req,res){
    console.log(`hey you are in home controller!!`);
     return res.end('<h1>Express is up for codeial</h1>');
 }
+// module.exports.actionName =function(req,res){
+
+//}
