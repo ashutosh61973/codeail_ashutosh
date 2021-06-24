215852552a87cd39ba73a2c31d8c9c8458b14de0
Ashutosh Dixit <ashutoshd2001@gmail.com>
2021-06-24 13:37:45 +0530

    setting routers

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router 
+app.use('/', require('./routes'));
 
-
-
-app.listen(port, function (err) {
-    if (err) {
-        console.log(`Error in running the server: ${err}`);
+app.listen(port, function (errr) {
+    if (errr) {
+        console.log(`Errror in running the server: ${errr}`);
     }
     console.log(`Server on running on port : ${port}`);
 });
