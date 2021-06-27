348dd90866a382ad696c4d99fd20207d5cb999b8
Ashutosh Dixit <ashutoshd2001@gmail.com>
2021-06-27 14:06:45 +0530

    adding assest

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 module.exports.home=function(req,res){
-   console.log(`hey you are in home controller!!`);
+//    console.log(`hey you are in home controller!!`);
     // return res.end('<h1>Express is up for codeial</h1>');
     return res.render('home',{
         title: "Home"
