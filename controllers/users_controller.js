47bd8aa0f426b7aa7208a08c3e2615997890ec04
Ashutosh Dixit <ashutoshd2001@gmail.com>
2021-07-04 09:44:08 +0530

    user sign up/in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User=require('../models/user')
+
+
 module.exports.profile=function(req,res){
     // res.send('<h1>user profile</h1>');
     res.render('user_profile',{
@@ -22,7 +25,27 @@ module.exports.signIn = function(req,res)
 // get sign up data
 module.exports.create=function(req,res)
 {
-    //something
+    // console.log(req);
+    if(req.body.password!=req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }
+    User.findOne({email : req.body.email},function(err,user){
+
+        if(err){console.log('error is finding user in signing up');return}
+        // console.log(`user-param :  ${user}`);
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('error is finding user in signing up');return}
+                
+                return res.redirect('/users/sign-in')
+            })
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    })
 }
 // sign in and create  
 module.exports.createSession=function(req,res)
--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 const userSchema =new mongoose.Schema({
     email: {
         type :String,
